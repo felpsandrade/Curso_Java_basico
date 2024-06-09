ebac58eb7364d78433e0a5b9ec28b8fefc03849a
Felipe Andrade <138839013+felpsandrade@users.noreply.github.com>
2024-06-09 13:02:14 -0300

    Add files via upload
    Sintaxes do java
    -Anatomia de Classes
    -Tipos Variaveis
    -Operadores
    -Metodos
    -Escopos
    Etc...

---
+++ java-sintaxe/src/edu/felipe/anatomiaClasses/MinhaClasse.java
@@ -0,0 +1,32 @@
+/*public class MinhaClasse {
+    public static void main(String [] args){
+
+        System.out.print("Hello World, Welcome!");
+        // Aprendendo a declarar variaveis
+
+        // Essa Variavel pode ter o valor editado ao longo do codigo, e deve começar minuscula e os outros 1ªMaiuscula
+        int ano = 2021;
+        ano = 2022;
+        int anoFabricacao = 2000;
+
+        // Quando a variavel tem 'final' ela tem o valor imutavel, e deve sempre ser declarada com letra MAIUSCULA
+        // Quanto já tiver a letra MAIUSCULA, já é entendido que é uma variavel imutavel
+        final String BR = "Brasil";
+        double PI = 3.14;
+
+
+        // Declação inválida de variáveis
+
+        int numero&um = 1; //Os únicos símbolos permitidos são _ e $
+        int 1numero = 1;    //Uma variável não pode começar com númerico
+        int numero um = 1; //Não pode ter espaço no nome da variável
+        int long = 1; //long faz parte das palavras reservadas da linguagem
+
+        // Declaração válida de veriáveis
+        int numero$um = 1;
+        int numero1 = 1;
+        int numeroum = 1;
+        int longo = 1;
+
+    }
+}*/
---
+++ java-sintaxe/src/edu/felipe/anatomiaClasses/MinhaClasse2.java
@@ -0,0 +1,27 @@
+/*public class MinhaClasse2 {
+    public static void main(String[] args) {
+
+        // Declarar uma variável em Java segue sempre a seguinte estrutura:
+
+        //Estrutura
+
+        Tipo NomeBemDefinido = Atribuição (opcional em alguns casos)
+
+        // Exemplo
+        int idade = 23;
+        double altura = 1.62;
+        Dog spike; //observe que aqui a variável spike não tem valor, é normal
+
+        // Declarando métodos em Java segue uma estrutura bem simples:
+
+        // Estrutura
+
+        TipoRetorno NomeObjetivoNoInfinitivo Parametro(s)
+
+        //Exemplo
+        int somar (int numeroUm, int numero2)
+        String formatarCep (long cep)
+
+    }
+
+}*/
