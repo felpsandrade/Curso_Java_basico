63d5c84b7ad1b4aabbcd8168592c93d3dedf7a5d
Felipe Oliveira <felipeandr2004@outlook.com>
2024-06-09 14:38:25 -0300

    Teste variaveis

--- java-sintaxe/src/edu/felipe/tiposvariaveis/tiposvariaveis.java
+++ java-sintaxe/src/edu/felipe/tiposvariaveis/tiposvariaveis.java
@@ -1,13 +1,18 @@
-package edu.felipe.tiposVariaveis;
+package edu.felipe.tiposvariaveis;
 
-public class tiposVariaveis {
+public class tiposvariaveis {
+
         public static void main(String[] args) {
-            byte idade = 123;
+            /*byte idade = 123;
             short ano = 2021;
             int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
             long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
             float pi = 3.14F;
-            double salario = 1275.33;
+            double salario = 1275.33;*/
+
+            int numero = 5;
+            numero = 10;
+
+            System.out.print(numero);
         }
     }
-}
