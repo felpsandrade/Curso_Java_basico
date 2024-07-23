7b938fad2fc0c1f522795b44106940783c17139b
Felipe Oliveira <felipeandr2004@outlook.com>
2024-07-23 00:03:59 -0300

    operadores

---
+++ java-sintaxe/src/edu/felipe/operadores/operadores.java
@@ -0,0 +1,12 @@
+package edu.felipe.operadores;
+
+public class operadores {
+    //Atribuição: Representado pelo símbolo de igualdade '='
+
+    String nome = "GLEYSON";
+    int idade = 22;
+    double peso = 68.5;
+    char sexo = 'M';
+    boolean doadorOrgao = false;
+    Date dataNascimento = new Date();
+}
