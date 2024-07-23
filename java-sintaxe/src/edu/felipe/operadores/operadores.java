e3d105e092fe66ba7bf1349cbd0a23fca11bc9aa
Felipe Oliveira <felipeandr2004@outlook.com>
2024-07-23 00:13:12 -0300

    operadores

--- java-sintaxe/src/edu/felipe/operadores/operadores.java
+++ java-sintaxe/src/edu/felipe/operadores/operadores.java
@@ -1,6 +1,8 @@
 package edu.felipe.operadores;
 
 public class operadores {
+    /*SÃO SÍMBOLOS ESPECIAIS QUE TEM UM SIGNIFICADO PRÓPRIO PARA A LINGUAGEM E ESTÃO ASSOCIADOS A DETERMINADAS OPERAÇÕES*/
+
     //Atribuição: Representado pelo símbolo de igualdade '='
 
     String nome = "GLEYSON";
@@ -8,5 +10,5 @@ public class operadores {
     double peso = 68.5;
     char sexo = 'M';
     boolean doadorOrgao = false;
-    Date dataNascimento = new Date();
+
 }
